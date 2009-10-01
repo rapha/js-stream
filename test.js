b8a5669f4761e828b11c76e163ad29c8968ede69
Raphael Speyer <rspeyer@gmail.com>
2009-10-02 00:55:32 +1000

    updated examples

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-#!/usr/bin/env rhino -version 180
+#!/usr/bin/env es5
 
 load('stream.js')
+
+// EXAMPLES
 
 // simple stream
 var one = (function() { while (true) yield 1 })()
@@ -9,15 +11,14 @@ var one = (function() { while (true) yield 1 })()
 var all = function(x) { return (function () { while (true) yield x })() }
 var from = Stream.unfold(function(x) x + 1 );
 var square = Stream.unfold(function(x) x * x );
-var triple = Stream.unfold(function(x) x * 3 );
+
 var lines = function(filename) {
-  var reader = new java.io.BufferedReader(new java.io.FileReader(filename));
-  return (function() {
+  return (function(reader) {
     for (var line = reader.readLine(); line; line = reader.readLine()) {
       yield line;
     }
     reader.close();
-  })()
+  })(new java.io.BufferedReader(new java.io.FileReader(filename)))
 }
 
 var fib = (function() {
@@ -30,33 +31,33 @@ var fib = (function() {
 })()
 
 
+var begins = function(stream, arr) {
+  org.junit.Assert.assertEquals(arr, stream.take(arr.length));
+}
 
-// tests
-var assertEquals = org.junit.Assert.assertEquals;
+begins(fib, [1,1,2,3,5])
 
-assertEquals([1,1,2,3,5], fib.take(5))
+begins(one, [1,1,1]);
+begins(all(3), [3,3,3]);
+begins(from(1), [1,2,3]);
+begins((function() { var s = from(1); s.take(1); return s})(), [2,3,4]);
+begins(square(3), [3,9,81]);
+begins(from(1).skip(2), [3,4,5]);
+begins(from(0).filter(function(val) val % 2 == 0), [0,2,4]);
+begins(from(1).map(function(x) x*x), [1,4,9]);
+begins(from(1).map(function(x) x*x).map(Math.sqrt), [1,2,3]);
 
-assertEquals([1,1,1], one.take(3));
-assertEquals([3,3,3], all(3).take(3));
-assertEquals([1,2,3], from(1).take(3));
-assertEquals([4,16,256], square(4).take(3));
-assertEquals([2,6,18], triple(2).take(3));
-assertEquals([54,162,486], (function() { var iter = triple(2); iter.take(3); return iter.take(3); })() );
-assertEquals([0,2,4], from(0).filter(function(val) val % 2 == 0).take(3));
-assertEquals([1,4,9], from(1).map(function(x) x*x).take(3));
-assertEquals([3,4,5], from(1).skip(2).take(3));
+begins(lines("test.js").skip(2), ["load('stream.js')"]);
 
-assertEquals(["load('stream.js')"], lines("test.js").skip(2).take(1));
+begins(all(3).add(all(2)), [5,5,5]);
+begins(all(3).interleave(all(2)), [3,2,3,2]);
+begins(from(1).interleave(from(4)), [1,4,2,5]);
 
-assertEquals([5,5,5], all(3).add(all(2)).take(3));
-assertEquals([3,2,3,2], all(3).interleave(all(2)).take(4));
-assertEquals([1,4,2,5], from(1).interleave(from(4)).take(4));
-
-var zipped = from(1).zip(all('a')).take(2);
+var actual = from(1).zip(all('a')).take(2);
 [[1,'a'], [2,'a']].forEach(function(expected, i) {
-    assertEquals(expected, zipped[i]);
+  org.junit.Assert.assertEquals(expected, actual[i]);
 })
 
 var days = function() { yield "mon"; yield "tue"; yield "wed"; yield "thu"; yield "fri"; }
 
-assertEquals(["mon", "tue", "wed"], days().take(3));
+begins(days(), ["mon", "tue", "wed"]);
