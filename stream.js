d74fc3eda59a0c6503684b71efca3ad01dd75a91
Raphael Speyer <rspeyer@gmail.com>
2010-08-17 01:40:14 +1000

    converted all methods to be on the Stream object

--- stream.js
+++ stream.js
@@ -1,86 +1,94 @@
-var Stream = (function(){yield 1})().__proto__.constructor
+var Stream = Object.getPrototypeOf(function(){yield 1}()).constructor
 
 Stream.unfold = function(func) {
   return function(seed) {
-    return (function() {
-      for (var next = seed; true; next = func(next)) {
-        yield next;
-      }
-    })()
+		for (let next = seed; true; next = func(next)) {
+			yield next;
+		}
   }
 }
 
 Stream.count = Stream.unfold(function(x) x + 1 );
 Stream.repeat = Stream.unfold(function(x) x);
-Stream.cycle = function(arr) {
-	var i = 0;
-	while (true) {
+Stream.cycle = function(stream) {
+	var vals = [];
+	try {
+		while (true) {
+			let then = stream.next();
+			vals.push(then);
+			yield then;
+		}
+	} catch (e if e === StopIteration) {
+		let i = 0;
+		while (true) {
+			yield vals[i];
+			i = (i+1) % vals.length;
+		}
+	}
+}
+Stream.stream = function(arr) {
+	for (var i = 0; i < arr.length; i++) {
 		yield arr[i];
-		i = (i+1) % arr.length
+	}
+	throw StopIteration;
+}
+
+Stream.take = function(stream) {
+	return function(n) {
+		var list = new Array(n);
+		for (var i = 0; i < n; i += 1) {
+			list[i] = stream.next();
+		}
+		return list;
 	}
 }
 
-
-Stream.prototype.take = function(n) {
-  var list = new Array(n);
-  for (var i = 0; i < n; i += 1) {
-    list[i] = this.next();
-  }
-  return list;
+Stream.drop = function(stream) {
+	return function(n) {
+		for (var i = 0; i < n; i++) {
+			stream.next();
+		}
+		return stream;
+	}
 }
 
-Stream.prototype.drop = function(n) {
-  for (var i = 0; i < n; i++) {
-    this.next();
-  }
-  return this;
+Stream.interleave = function() {
+  var iters = Array.prototype.concat.apply([], arguments);
+	for (var i = 0; true; i++) {
+		yield iters[i % iters.length].next();
+	}
 }
 
-Stream.prototype.interleave = function() {
-  var iters = Array.prototype.concat.apply([this], arguments);
-  return (function() {
-    for (var i = 0; true; i++) {
-      yield iters[i % iters.length].next();
-    }
-  })()
+Stream.zip = function() {
+  var iters = Array.prototype.concat.apply([], arguments);
+	while (true) {
+		yield iters.map(function(iter) iter.next());
+	}
 }
 
-Stream.prototype.add = function() {
-  var iters = Array.prototype.concat.apply([this], arguments);
-  return (function() {
-    while (true) {
-      var sum = 0;
-      iters.forEach(function(iter) { sum += iter.next() });
-      yield sum;
-    }
-  })()
-},
-
-Stream.prototype.zip = function() {
-  var iters = Array.prototype.concat.apply([this], arguments);
-  return (function() {
-    while (true)
-      yield iters.map(function(iter) { return iter.next() });
-  })()
+Stream.combine = function(combiner, acc) {
+	return function() {
+		var args = [].concat.apply([], arguments);
+		var groups = Stream.zip.apply(null, args);
+		return Stream.map(function(values) values.reduce(combiner, acc))(groups);
+	}
 }
 
-Stream.prototype.filter = function(predicate) {
-  var self = this;
-  return (function() {
+Stream.filter = function(predicate) {
+  return function(stream) {
       while (true) {
-        var value = self.next();
+        var value = stream.next();
         if (predicate(value)) {
           yield value;
         }
       }
-  })()
+  }
 }
 
-Stream.prototype.map = function(transform) {
-  var self = this;
-  return (function() {
-      while (true) {
-        yield transform(self.next());
-      }
-  })()
+Stream.map = function(transform) {
+	return function(stream) {
+		while (true) {
+			yield transform(stream.next());
+		}
+	}
 }
--- test.js
+++ test.js
@@ -6,15 +6,32 @@ load('stream.js')
 
 // simple stream
 // some stream builders
-var square = Stream.unfold(function(x) x * x );
+var stream = Stream.stream,
+		unfold = Stream.unfold,
+		repeat = Stream.repeat,
+		count = Stream.count,
+		cycle = Stream.cycle,
+		take = Stream.take,
+		drop = Stream.drop,
+		interleave = Stream.interleave,
+		map = Stream.map,
+		filter = Stream.filter,
+		combine = Stream.combine,
+		zip = Stream.zip;
+
+var add = combine(function(a,b) a + b, 0);
+
+var square = function(x) { return x * x; };
+
+var doubling = unfold(function(x) x + x );
 
 var lines = function(filename) {
-  return (function(reader) {
-    for (var line = reader.readLine(); line; line = reader.readLine()) {
-      yield String(line);
-    }
-    reader.close();
-  })(new java.io.BufferedReader(new java.io.FileReader(filename)))
+	let reader = new java.io.BufferedReader(new java.io.FileReader(filename));
+	for (let line = reader.readLine(); line; line = reader.readLine()) {
+		yield String(line);
+	}
+	reader.close();
+	throw StopIteration;
 }
 
 var fib = (function() {
@@ -29,31 +46,42 @@ var fib = (function() {
 
 var begins = function(stream, arr) {
 	var expected = arr.toSource(),
-			actual = stream.take(arr.length).toSource();
+			actual = take(stream)(arr.length).toSource();
 	if (expected !== actual) throw 'Expected '+expected+' but was '+actual+'.'
+}
+
+var becomes = function(stream, arr) {
+	begins(stream, arr);
+	try {
+		var then = stream.next();
+		throw 'Expected stream to be exhausted, but still had ' + then;
+	} catch (e if e === StopIteration) {
+		return; // success
+	}
 }
 
 begins(fib, [1,1,2,3,5])
 
-begins(Stream.repeat(3), [3,3,3]);
-begins(Stream.count(1), [1,2,3]);
-begins((function() { var s = Stream.count(1); s.take(1); return s})(), [2,3,4]);
-begins(square(3), [3,9,81]);
-begins(Stream.count(1).drop(2), [3,4,5]);
-begins(Stream.count(0).filter(function(val) val % 2 == 0), [0,2,4]);
-begins(Stream.count(1).map(function(x) x*x), [1,4,9]);
-begins(Stream.count(1).map(function(x) x*x).map(Math.sqrt), [1,2,3]);
+begins(repeat(3), [3,3,3]);
+begins(count(1), [1,2,3]);
+begins((function() { var s = count(1); take(s)(1); return s})(), [2,3,4]);
+begins(doubling(3), [3,6,12]);
+begins(drop(count(1))(2), [3,4,5]);
+begins(filter(function(val) val % 2 == 0)(count(0)), [0,2,4]);
+begins(map(square)(count(1)), [1,4,9]);
+begins(map(Math.sqrt)(map(square)(count(1))), [1,2,3]);
 
-begins(lines("test.js").drop(2), ["load('stream.js')"]);
+begins(drop(lines("test.js"))(2), ["load('stream.js')"]);
 
-begins(Stream.repeat(3).add(Stream.repeat(2)), [5,5,5]);
-begins(Stream.repeat(3).interleave(Stream.repeat(2)), [3,2,3,2]);
-begins(Stream.count(1).interleave(Stream.count(4)), [1,4,2,5]);
+begins(add(repeat(3), repeat(2)), [5,5,5]);
+begins(interleave(repeat(3), repeat(2)), [3,2,3,2]);
+begins(interleave(count(1), count(4)), [1,4,2,5]);
 
-begins(Stream.count(1).zip(Stream.repeat('a')), [[1,'a'], [2,'a']]);
+begins(zip(count(1), repeat('a')), [[1,'a'], [2,'a']]);
 
 var days = function() { yield "mon"; yield "tue"; yield "wed"; yield "thu"; yield "fri"; }
 
 begins(days(), ["mon", "tue", "wed"]);
 
-begins(Stream.cycle([1,2,3]), [1,2,3,1,2,3,1]);
+becomes(stream([1,2,3]), [1,2,3]);
+begins(cycle(stream([1,2,3])), [1,2,3,1,2,3,1]);
